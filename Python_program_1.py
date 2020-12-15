89e7517bc7227c4922c6ae739d548502ee2535c5
Pranali mate <66291863+pranali0127@users.noreply.github.com>
2020-12-15 18:27:19 +0530

    Created first python program
    This is a simple python program that contains an introduction of numbers in python. It also shows some basic mathematical operations such as addition(+), subtraction(-), multiplication(*), division(/), integer division(//) ,modulus function(%).

---
+++ Python_program_1.py
@@ -0,0 +1,45 @@
+#This program works with interger and float
+
+#initialising two int variables
+num1 = 12
+num2 = 10
+print("num1 = ",num1)
+print("num2 = ",num2)
+
+#initialising two float variables
+num3 = 12.345
+num4 = 3.098
+print("num3 = ",num3)
+print("num4 = ",num4)
+
+#checking the type of variables using type() function
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(num4))
+
+#mathematical operations of integer variable
+print( num1+num2 )#addition
+print( num1-num2 )#subtraction
+print( num1*num2 )#multiplication
+print( num1/num2 )#division
+print( num1//num2 )#integer division
+print( num1%num2 )#modulus opearator( this gives remainder )
+
+#mathematical operations of float variable
+print( num3+num4 )#addition
+print( num3-num4 )#subtraction
+print( num3*num4 )#multiplication
+print( num3/num4 )#division
+print( num3//num4 )#integer division
+print( num3%num4 )#modulus opearator( this gives remainder )
+
+#mathematical operations of integer and float variables
+print( num1+num3 )#addition
+print( num2-num4 )#subtraction
+print( num1*num3 )#multiplication
+print( num2/num4 )#division
+print( num1//num3 )#integer division
+print( num2%num4 )#modulus opearator( this gives remainder )
+
+
