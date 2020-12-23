e1b441a751eadf08e48d87dafbfbe47edb3f9224
Pranali mate <66291863+pranali0127@users.noreply.github.com>
2020-12-23 16:23:28 +0530

    Create Python_program_8.py
    This program shows how we can concatenate two strings into one.

---
+++ Python_program_8.py
@@ -0,0 +1,8 @@
+#creating two strings
+string1 = "I Love "
+string2 = "Github"
+#for string concatenation we use + operator
+sentence = string1 + string2
+print( "Length of string1 :",string1,len(string1) )
+print( "Length of string2 :",string2 ,len(string2) )
+print( "Length after concatination :",sentence, len(sentence) )
